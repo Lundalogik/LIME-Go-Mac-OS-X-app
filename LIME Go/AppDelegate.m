6f85da13f2b461187963db22a9c2dad9ce96fc91
Filip Persson <filip.persson@me.com>
2014-03-25 21:34:00 +0100

    Fixed user-agent

--- LIME Go/AppDelegate.m
+++ LIME Go/AppDelegate.m
@@ -16,7 +16,18 @@
     NSURL *url = [NSURL URLWithString:urlAddress];
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
     
+    //The webviews useragent is crazy, this is a ugly hack to fix it:
+    [webview setCustomUserAgent: @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.74.9 (KHTML, like Gecko) Version/7.0.2 Safari/537.74.9"];
+    
     [[webview mainFrame] loadRequest:requestObj];
+    
+    //Check for notifications every 30 second
+    [NSTimer scheduledTimerWithTimeInterval:30
+                                     target:self
+                                   selector:@selector(handleNotifications)
+                                   userInfo:nil
+                                    repeats:YES];
+    
 }
 
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)theApplication hasVisibleWindows:(BOOL)flag
@@ -29,6 +40,13 @@
     }
     
     return YES;
+}
+
+-(void)handleNotifications
+{
+    
+    //Update the dock badge
+    //[[[NSApplication sharedApplication] dockTile]setBadgeLabel:@"2234"];
 }
 
 
